963e3a7c933b1661c5cc9d1ebde16933cdc683c8
Sara Basile <sara@snbasile.com>
2022-11-21 16:52:32 -0500

    updated vendor test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Vendor.create!([
     fullname: 'Bill Murray',
     email: 'bill@murray.com',
     instagram: 'billm1',
-    thumbnail: 'https://fillmurray.com/100/100',
+    thumbnail: 'https://www.fillmurray.com/100/100',
     desc: 'For relaxing times, make it Suntory time.',
     password: 'Bill123#'
 },
@@ -21,16 +21,16 @@ Vendor.create!([
     fullname: 'Nicolas Cage',
     email: 'nick@cage.com',
     instagram: 'nickc1',
-    thumbnail: 'https://placecage.com/100/100',
-    desc: 'I want to be Bob Denver on acid playing the accordion',
+    thumbnail: 'https://www.placecage.com/100/100',
+    desc: 'I want to be Bob Denver on acid playing the accordion.',
     password: 'Nick456#'
 },
 {
     fullname: 'Steven Segal',
     email: 'steve@segal.com',
     instagram: 'steves1',
-    thumbnail: 'https://stevensegallery.com/100/100',
-    desc: 'I want to be Bob Denver on acid playing the accordion',
+    thumbnail: 'https://www.stevensegallery.com/100/100',
+    desc: 'Any great warrior is also a scholar, and a poet, and an artist.',
     password: 'Steve789#'
 }
 ])
