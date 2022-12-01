ea62ffe87ca819f5cfa2dd2e607aa7ee351dfca4
Sara Basile <sara@snbasile.com>
2022-12-01 18:43:06 -0500

    testing contact mailer templates

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,7 +1,9 @@
 class MessageMailer < ApplicationMailer
 
     default from: "OldSolFiesta@gmail.com"
-    default to: "OldSolFiesta@gmail.com"
+    # default to: "OldSolFiesta@gmail.com"
+    default to: "sara@snbasile.com"
+
 
 def contact_team(message)
   @message = message.textbody
