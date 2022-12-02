0491ce76e18112d761cc8cc559f44d9b59470a77
Sara Basile <sara@snbasile.com>
2022-12-02 11:24:55 -0500

    added winner bool to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_210326) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_161957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_210326) do
     t.datetime "dob"
     t.string "instagram"
     t.string "phone"
+    t.boolean "winner"
   end
 
   create_table "vendors", force: :cascade do |t|
